67d1bc4bcfbc58aa271a3310b55a5272a0aab17a
ADRIAN <adriangallery@adriancerda.com>
2025-02-13 13:55:11 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,12 +6,13 @@ import os
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 TELEGRAM_GROUP_ID = int(os.getenv("TELEGRAM_GROUP_ID"))
 DISCORD_WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_URL")
+BOT_COMPRAS_ID = int(os.getenv("BOT_COMPRAS_ID"))  # ID del bot de compras
 
 bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
 
-@bot.message_handler(func=lambda message: message.chat.id == TELEGRAM_GROUP_ID)
+@bot.message_handler(func=lambda message: message.chat.id == TELEGRAM_GROUP_ID and message.from_user.id == BOT_COMPRAS_ID)
 def forward_to_discord(message):
-    data = {"content": f"📢 **Nuevo mensaje en Telegram:**\n\n{message.text}"}
+    data = {"content": f"📢 **Nuevo mensaje del bot de compras:**\n\n{message.text}"}
     requests.post(DISCORD_WEBHOOK_URL, json=data)
 
 bot.polling()
